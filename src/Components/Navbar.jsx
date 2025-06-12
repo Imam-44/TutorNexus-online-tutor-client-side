a38377a014ee8c27dc1c94dac8df5da16b7de4a1
Imam Hasan <imamets3@gmail.com>
2025-06-13 01:29:11 +0600

    added navbar and setup ract router

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
         <div className="navbar max-w-11/12 mx-auto bg-base-100 ">
           <div className="navbar-start">
             <div className="dropdown">
-              <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <div tabIndex={0} role="button" className="btn btn-ghost text-fuchsia-500  lg:hidden">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
               </div>
               <ul
